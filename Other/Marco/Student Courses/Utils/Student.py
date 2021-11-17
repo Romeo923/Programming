eed458665dabcc25a0cb2018395c87822cdb226c
MrSuave <cromeo5112@gmail.com>
2021-11-16 19:42:48 -0500

    Merge branch 'master' of https://github.com/Romeo923/Programming

---
+++ Other/Marco/Student Courses/Utils/Student.py
@@ -0,0 +1,47 @@
+class Course:
+
+    def __init__(self,name,id,credits,semester,registered):
+        self.name = name
+        self.id = id
+        self.credits = credits
+        self.semester = semester
+        self.registered = registered
+
+    def __str__(self):
+        return f"""
+        Course Name:   {self.name}
+        Course Number: {self.id}
+        Credits:       {self.credits}
+        Semester:      {self.semester}
+        Registered:    {self.registered}
+        """
+
+
+class Student:
+
+    def __init__(self,name,id,courses):
+        self.name = name
+        self.ID = id
+        self.courses = [Course(name=data["name"],id=data["ID"],credits=data["credits"],semester=data["semester"],registered=data["registered"]) for data in courses]
+
+    def adddCourse(self,course):
+        self.courses.append(course)
+
+    def fallCourses(self):
+        return (course for course in self.courses if course.semester == 'Fall')
+    
+    def springCourses(self):
+        return (course for course in self.courses if course.semester == 'Spring')
+
+    def __str__(self):
+        
+        student_str = f"""
+        Student Name:   {self.name}
+        Student ID:     {self.ID}
+        Courses:
+        ---------------------------
+        """
+        for course in self.courses:
+            student_str += f'{course}\n'
+
+        return student_str
---
+++ Other/Marco/Student Courses/studentCourses.py
@@ -0,0 +1,57 @@
+import json
+from Utils.Student import *
+
+file_path = 'Other\\Marco\\Student Courses\\Utils\\student_data.json'
+
+def save(student_list):
+    students = []
+    for student in student_list:
+        courses = []
+        for course in student.courses:
+            courses.append({
+                "name":course.name,
+                "ID":course.id,
+                "semester":course.semester,
+                "credits":course.credits,
+                "registered":course.registered
+            })
+
+        students.append({
+            "name":student.name,
+            "ID":student.ID,
+            "courses":courses
+        })
+
+    with open(file_path,'w') as file:
+        json.dump({"Students":students},file,indent=2)
+
+with open(file_path) as file:
+    students = json.load(file)["Students"]
+
+slist = [Student(name=data["name"],id=data["ID"],courses=data["courses"]) for data in students]
+
+s1 = Student("Marco",10000,[{
+        "name":"Test",
+        "ID":"Test-500",
+        "semester":"Spring",
+        "credits":3,
+        "registered":True        
+    }])
+
+# slist.append(s1)
+
+
+
+# for s in slist:
+#     print(s)
+#     print('-=-=-=-=-=-=-=-=-=-=-=-=-')
+    # print("Fall\n")
+    # for course in s.fallCourses():
+    #     print(course)
+    #     print("-----------------------")
+
+    # print("Spring\n")       
+    # for course in s.springCourses(): 
+    #     print(course)
+    #     print("-----------------------")
+    # print('-=-=-=-=-=-=-=-=---=-=-=-=-=-=-=-=-')
