0d9ecae09da560b6766c0f12480c7467b7c96a61
MrSuave <cromeo5112@gmail.com>
2021-11-20 00:58:53 -0500

    l

--- Other/Marco/Student Courses/studentCourses.py
+++ Other/Marco/Student Courses/studentCourses.py
@@ -1,9 +1,8 @@
 from Utils import *
 
 def main():
-    
-    createFrame(file_path = 'student_data.json')
 
+    createFrame(file_path = 'student_data.json')
 
 if __name__ == '__main__':
     main()
