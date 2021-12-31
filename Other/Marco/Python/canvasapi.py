f126f6f7a0c87c0060bd4172ee4674dd7b4ebd8a
Mr.Suave <cromeo5112@gmail.com>
2021-12-31 01:13:28 -0500

    create assignments

--- Other/Marco/Python/canvasapi.py
+++ Other/Marco/Python/canvasapi.py
@@ -2,30 +2,112 @@ import pandas as pd
 import requests
 
 token = '19~oYg0RXgAQTv8YVsrSdUZfb0kmFVNsdu8z4CSoDLDvcsLhUoRnidKjR8SXDmDtIDu'
+headers = {"Authorization": f"Bearer {token}"}
 
-# grades = pd.read_csv('Other\Marco\Python\grades.csv')
+ub_url = f'https://bridgeport.instructure.com/api/v1/'
 
-# student, ID, SIS_Login, section, name = grades
-# points_possible = grades[name][0]
+grades = pd.read_csv('Other\Marco\Python\\testing.csv')
 
-# for i in range(1,len(grades)):
-#     student_name = grades[student][i]
-#     student_id = grades[ID][i]
-#     student_sis = grades[SIS_Login][i]
-#     course_section = grades[section][i]
-#     assignment_name = grades[name][i]
+student, ID, SIS_Login, section, assignment = grades
+points_possible = grades[assignment][0]
+
+
+def createAssignment(course_id, data):
+    full_path = f'{ub_url}courses/{course_id}/assignments/'
+    return requests.post(url=full_path,headers=headers,params=data)
+
+def getAssignmentID(course_id):
+    full_path = f'{ub_url}courses/{course_id}/assignments/'
+    response = requests.get(url=full_path,headers=headers)
+    assignments = response.json()
+    for a in assignments:
+        if a['name'] == assignment:
+            return a['id']
+    return -1
+
+def gradeAssignment(course_id, assignment_id, user_id, data):
+    full_path = f'{ub_url}courses/{course_id}/assignments/{assignment_id}/submissions/{user_id}'
+    requests.put(url=full_path,headers=headers,params=data)
+
+def getAssignmentGroup(course_id, name):
+    full_path = f'{ub_url}courses/{course_id}/assignment_groups/'
+    response = requests.get(url=full_path,headers=headers)
+    groups = response.json()
+
+    for group in groups:
+        if name in group['name']:
+            print(group['name'])
+            print(group['id'])
+            return group
+    
+    return None
+
+def main():
+
+    #get course id
+    course_id = '1865191'
+
+    #check if assignment exists, if not, create it
+    assignment_id = getAssignmentID(course_id)
+
+    if assignment_id == -1:
+
+        data = {
+        'assignment[name]': assignment,
+        'assignment[points_possible]' : points_possible,
+        'assignment[published]' : True
+        }
+
+        possible_groups = ['Homework', 'Quiz']
+
+        name = 'Has No Group'
+        for g in possible_groups:
+            name = g if g in assignment else name
+
+        group = getAssignmentGroup(course_id, name)
+
+        str= f'Created Assignment: {assignment}'
+
+        if group is not None:
+            data['assignment[assignment_group_id]'] = group['id']
+            str+= f' in group: {group["name"]}'
+
+        assignment_id = createAssignment(course_id,data).json()['id']
+        
+        print(str)
+
+    print(assignment_id)
+
+
+    for i in range(1,len(grades)):
+        student_name = grades[student][i]
+        student_id = grades[ID][i]
+        student_sis = grades[SIS_Login][i]
+        course_section = grades[section][i]
+        assignment_grade = grades[assignment][i]
+
+        data = {
+            'submission[posted_grade]' : assignment_grade
+        }
+
+        response = gradeAssignment(course_id, assignment_id, student_id, data)
+        print(response)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 course_id = '1865191'
-# assignment_id = ''
-# user_id = ''
-url = f'https://bridgeport.instructure.com/api/v1/courses/{course_id}/assignments/'
+assignment_id = '14717796'
 
-# data = {}
-headers = {"Authorization": f"Bearer {token}"}
 
-result = requests.get(url=url,headers=headers)
-# result = requests.post(url=url,headers=headers,)
-print(result.json())
 
-# result = requests.post(url=url,headers=headers,data=data)
