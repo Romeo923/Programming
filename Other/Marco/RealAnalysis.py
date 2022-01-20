517363c8e0b5024022068097b1592e7755405954
MrSuave <cromeo5112@gmail.com>
2022-01-19 20:11:01 -0500

    l

--- Other/Marco/RealAnalysis.py
+++ Other/Marco/RealAnalysis.py
@@ -1,7 +1,37 @@
+
+class Set:
+    def __init__(self,n=None):
+        self.elements = [] if n is None else list(n.elements)
+    
+    def __str__(self):
+        # if len(self.elements) == 0:
+        #     return 'Ø'
+        
+        s = '{'
+
+        for n in self.elements:
+            s += f"{n},"
+
+        if len(self.elements) > 0:
+            s = s[:-1]
+        s += '}'
+        
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+            
+    
+    def __or__(self,n):
+        s = Set(self)
+        s.elements.append(n)
+        return s
+        
+    
+
 class TheNaturals:
     def __init__(self):
-        self.empty = 'Ø'
-        self.naturals = {0:self.empty}
+        self.naturals = {0:Set()}
         self.max = 0
     
     def __getitem__(self,n):
@@ -11,15 +41,12 @@ class TheNaturals:
         
         if n+1 > self.max:    
             N = self.successor(n-1)
-            # self.naturals[n+1] = 
+            self.naturals[n+1] = N | Set(N)
             self.max = n+1
             
         return self.naturals[n+1]
     
-    
-THE_NATURALS = TheNaturals()
-print(THE_NATURALS[0])
-print(THE_NATURALS[1])
-print(THE_NATURALS[2])
-print(THE_NATURALS[3])
-print(THE_NATURALS.naturals)
+
+the_naturals = TheNaturals()
+
+print(the_naturals[15])
