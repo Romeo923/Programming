8d3570c2ec1c211cab9acd1d2c099debc37aeebb
MrSuave <cromeo5112@gmail.com>
2022-02-23 23:57:42 -0500

    l

---
+++ CV Programs/Assignment 5/CV Assignment 5/CV Assignment 5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV_Assignment_5
+{
+
+    static class Program
+    {
+        
+        //[STAThread]
+
+
+        static List<Point> Shape1 = new List<Point>();
+        static List<Point> Shape2 = new List<Point>();
+
+        private static void btnInitializeShapes_Click(object sender, EventArgs e)
+        {
+            Shape1.Clear();
+            Shape2.Clear();
+            Point p1a = new Point(20, 30);
+            Point p2a = new Point(120, 50);
+            Point p3a = new Point(160, 80);
+            Point p4a = new Point(180, 300);
+            Point p5a = new Point(100, 220);
+            Point p6a = new Point(50, 280);
+            Point p7a = new Point(20, 140);
+            Shape1.Add(p1a);
+            Shape1.Add(p2a);
+            Shape1.Add(p3a);
+            Shape1.Add(p4a);
+            Shape1.Add(p5a);
+            Shape1.Add(p6a);
+            Shape1.Add(p7a);
+            Transformation T2 = new Transformation();
+            T2.A = 1.05; T2.B = 0.05; T2.T1 = 15; T2.T2 = 22;
+            Shape2 = ApplyTransformation(T2, Shape1);
+            Shape2[2] = new Point(Shape2[2].X + 10, Shape2[2].Y + 3);// change one point
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape1.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2, pRed, g);
+        }
+
+        static void DisplayShape(List<Point> Shp, Pen pen, Graphics g)
+        {
+            Point? prevPoint = null; // nullable
+            foreach (Point pt in Shp)
+            {
+                g.DrawEllipse(pen, new Rectangle(pt.X - 2, pt.Y - 2, 4, 4));
+                if (prevPoint != null)
+                    g.DrawLine(pen, (Point)prevPoint, pt);
+                prevPoint = pt;
+            }
+            g.DrawLine(pen, Shp[0], Shp[Shp.Count - 1]);
+        }
+
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
