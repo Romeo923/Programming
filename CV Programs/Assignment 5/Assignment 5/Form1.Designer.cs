2fd7dd0d2b13956ec34c4b15dfa7d59d5321fd86
MrSuave <cromeo5112@gmail.com>
2022-02-25 11:22:20 -0500

    Assignment 5

---
+++ CV Programs/Assignment 5/Assignment 5/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Assignment_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panShape1 = new System.Windows.Forms.PictureBox();
+            this.panShape2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.panShape1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panShape2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panShape1
+            // 
+            this.panShape1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panShape1.Location = new System.Drawing.Point(106, 33);
+            this.panShape1.Name = "panShape1";
+            this.panShape1.Size = new System.Drawing.Size(438, 640);
+            this.panShape1.TabIndex = 0;
+            this.panShape1.TabStop = false;
+            // 
+            // panShape2
+            // 
+            this.panShape2.Location = new System.Drawing.Point(634, 33);
+            this.panShape2.Name = "panShape2";
+            this.panShape2.Size = new System.Drawing.Size(438, 640);
+            this.panShape2.TabIndex = 1;
+            this.panShape2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 679);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Initialize Shapes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(737, 679);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Apply Transformation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 765);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panShape2);
+            this.Controls.Add(this.panShape1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.panShape1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panShape2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox panShape1;
+        private System.Windows.Forms.PictureBox panShape2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CV Programs/Assignment 5/Assignment 5/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mapack;
+
+namespace Assignment_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static List<Point> Shape1 = new List<Point>();
+        static List<Point> Shape2 = new List<Point>();
+
+        private void initializeShapes(object sender, EventArgs e)
+        {
+            Shape1.Clear();
+            Shape2.Clear();
+            Point p1a = new Point(20, 30);
+            Point p2a = new Point(120, 50);
+            Point p3a = new Point(160, 80);
+            Point p4a = new Point(180, 300);
+            Point p5a = new Point(100, 220);
+            Point p6a = new Point(50, 280);
+            Point p7a = new Point(20, 140);
+            Shape1.Add(p1a); Shape1.Add(p2a);
+            Shape1.Add(p3a); Shape1.Add(p4a);
+            Shape1.Add(p5a); Shape1.Add(p6a);
+            Shape1.Add(p7a);
+            Transformation T2 = new Transformation
+            { A = 1.05, B = 0.05, T1 = 15, T2 = 22 };
+            Shape2 = ApplyTransformation(T2, Shape1);
+            Shape2[2] = new Point(Shape2[2].X + 10, Shape2[2].Y + 3);// change one point
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape1.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2, pRed, g);
+        }
+
+        void DisplayShape(List<Point> Shp, Pen pen, Graphics g)
+        {
+            Point? prevPoint = null; // nullable
+            foreach (Point pt in Shp)
+            {
+                g.DrawEllipse(pen, new Rectangle(pt.X - 2, pt.Y - 2, 4, 4));
+                if (prevPoint != null)
+                    g.DrawLine(pen, (Point)prevPoint, pt);
+                prevPoint = pt;
+            }
+            g.DrawLine(pen, Shp[0], Shp[Shp.Count - 1]);
+        }
+
+        public class Transformation
+        {
+            public double A { get; set; }
+            public double B { get; set; }
+            public double T1 { get; set; }
+            public double T2 { get; set; }
+        }
+
+        List<Point> ApplyTransformation(Transformation T, List<Point> shpList)
+        {
+            List<Point> TList = new List<Point>();
+            foreach (Point pt in shpList)
+            {
+                double xprime = T.A * pt.X + T.B * pt.Y + T.T1;
+                double yprime = T.B * pt.X * -1 + T.A * pt.Y + T.T2;
+                Point pTrans = new Point((int)xprime, (int)yprime);
+                TList.Add(pTrans);
+            }
+            return TList;
+        }
+
+        private void register(object sender, EventArgs e)
+        {
+            Matrix A = new Matrix(4, 4);
+            Matrix B = new Matrix(4, 1);
+
+
+            A[2, 2] = 2;
+            A[3, 3] = 2;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Point p1 = Shape1[i];
+                int x1 = p1.X;
+                int y1 = p1.Y;
+
+                Point p2 = Shape2[i];
+                int x2 = p2.X;
+                int y2 = p2.Y;
+
+                A[0, 0] += 2 * x2 * x2;
+                A[0, 0] += 2 * y2 * y2;
+
+                A[0, 2] += 2 * x2;
+                A[0, 3] += 2 * y2;
+
+                A[1, 1] += 2 * x2 * x2;
+                A[1, 1] += 2 * y2 * y2;
+                
+                
+                A[1, 2] += 2 * y2;
+                A[1, 3] += -2 * x2;
+
+                A[2, 0] += 2 * x2;
+                A[2, 1] += 2 * y2;
+
+                A[3, 0] += 2 * y2;
+                A[3, 1] += -2 * x2;
+
+                B[0, 0] += 2 * x1 * x2;
+                B[0, 0] += 2 * y1 * y2;
+
+                B[1, 0] += 2 * x1 * y2;
+                B[1, 0] -= 2 * x2 * y1;
+
+                B[2, 0] += 2 * x1;
+                B[3, 0] += 2 * x2;
+
+            }
+
+            Matrix result = A.Inverse * B;
+
+            Transformation t = new Transformation
+            { A = result[0, 0], B = result[1, 0], T1 = result[2, 0], T2 = result[3, 0] };
+
+            Shape2 = ApplyTransformation(t, Shape2);
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape2.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2, pRed, g);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initializeShapes(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            register(sender, e);
+        }
+    }
+}
