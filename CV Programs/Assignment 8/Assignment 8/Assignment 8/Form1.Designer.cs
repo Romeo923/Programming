0266c39baf5ddadc3a4552baf9e37b9e861ee56d
MrSuave <cromeo5112@gmail.com>
2022-02-27 11:35:20 -0500

    Finished assignment 8

--- CV Programs/Assignment 8/Assignment 8/Assignment 8/Form1.Designer.cs
+++ CV Programs/Assignment 8/Assignment 8/Assignment 8/Form1.Designer.cs
@@ -29,20 +29,290 @@ namespace Assignment_8
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(114, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(407, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(871, 81);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(631, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(407, 409);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(631, 409);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(871, 409);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(612, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(226, 46);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Top 3 Matches";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(612, 691);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(226, 39);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "First 3 Eigen Faces";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(80, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(246, 65);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Click Below To Add Image";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox8.Location = new System.Drawing.Point(114, 409);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox8.TabIndex = 10;
+            this.pictureBox8.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(80, 691);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(246, 51);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Eigen Reconstruction";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Enabled = false;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(407, 357);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(184, 46);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Enabled = false;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(631, 357);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(184, 46);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Enabled = false;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(871, 357);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(184, 46);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox9.Location = new System.Drawing.Point(1135, 233);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(184, 264);
+            this.pictureBox9.TabIndex = 15;
+            this.pictureBox9.TabStop = false;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Enabled = false;
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(1107, 176);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(246, 51);
+            this.textBox8.TabIndex = 16;
+            this.textBox8.Text = "Average Face";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1157, 759);
+            this.ClientSize = new System.Drawing.Size(1411, 756);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.TextBox textBox8;
     }
 }
 
--- CV Programs/Assignment 8/Assignment 8/Assignment 8/Form1.cs
+++ CV Programs/Assignment 8/Assignment 8/Assignment 8/Form1.cs
@@ -7,14 +7,286 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FaceRecogPCA;
 
 namespace Assignment_8
 {
     public partial class Form1 : Form
     {
+        
+        Matrix I; // pixels x number of images
+        Matrix average_face; // height x width
+        Matrix eig_faces; // pixels x number of eig faces
+        Matrix[] image_features; // number of eig faces x number of images
+
+        int image_width = 92;
+        int image_height = 112;
+        int classes = 40;
+        int images_per_class = 5;
+        int num_eig_faces = 30;
+
+        int size;
+        int number_of_images;
+
+
+        void loadImages()
+        {
+            string training_path = "C:/Users/Romeo/Desktop/Extra/Test Images/ATTFaceDataSet/Training/";
+
+            size = image_width * image_height;
+            number_of_images = classes * images_per_class;
+
+            I = new Matrix(size, number_of_images);
+            average_face = new Matrix(size, 1);
+            Bitmap average_bitmap = new Bitmap(image_width, image_height);
+
+            for(int current_class = 1, column_index = 0; current_class <= classes; current_class++)
+            {
+                
+                for(int current_image = 1;current_image <= images_per_class; current_image++)
+                {
+                    string image_path = training_path + "S" + current_class + "_" + current_image + ".jpg";
+                    Bitmap image = new Bitmap(image_path);
+
+                    for (int column = 0, row_index = 0; column < image.Width; column++)
+                    {
+                        for(int row = 0; row < image.Height; row++)
+                        {
+                            Color pixel = image.GetPixel(column, row);
+                            int value = pixel.R;
+                            I[row_index, column_index] = value;
+                            average_face[row_index, 0] += value;
+                            row_index++;
+                        }
+                    }
+                    column_index++;
+
+                }
+            }
+            
+            for(int i = 0, count = 0; i < image_width; i++)
+            {
+                for(int j = 0; j < image_height; j++)
+                {
+                    int pixel = (int)average_face[count, 0];
+                    pixel /= number_of_images;
+                    average_face[count++, 0] = pixel;
+                    average_bitmap.SetPixel(i, j, Color.FromArgb(pixel, pixel, pixel));
+                }
+            }
+            pictureBox9.Image = average_bitmap; 
+            
+            for (int i = 0; i < number_of_images; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    I[j, i] -= (int)average_face[j, 0];  // dimentions: pixels x images
+                }
+            }
+
+            Matrix cov = (Matrix)I.Transpose().Multiply(I); // dimentions: images x images
+            IEigenvalueDecomposition eig = cov.GetEigenvalueDecomposition();
+            Matrix eig_vectors = (Matrix)eig.EigenvectorMatrix; // dimentions: images x images
+            eig_vectors = (Matrix)eig_vectors.Submatrix(0, number_of_images-1, number_of_images-num_eig_faces, number_of_images-1); // dimentions: images x num_eig_faces
+            eig_faces = (Matrix)I.Multiply(eig_vectors); // dimentions: pixels x num_eig_faces
+            image_features = new Matrix[number_of_images];
+
+            for(int i = 0; i < number_of_images; i++)
+            {
+                Matrix curr_image = (Matrix)I.Submatrix(0, size-1, i, i);
+                Matrix features = (Matrix)curr_image.Transpose().Multiply(eig_faces); // dimentions: 1 x num_eig_faces
+                image_features[i] = features;
+            }
+
+            Bitmap img1 = new Bitmap(image_width, image_height);
+            Bitmap img2 = new Bitmap(image_width, image_height);
+            Bitmap img3 = new Bitmap(image_width, image_height);
+
+            int min1 = int.MaxValue;
+            int max1 = int.MinValue;
+            int min2 = int.MaxValue;
+            int max2 = int.MinValue;
+            int min3 = int.MaxValue;
+            int max3 = int.MinValue;
+            for (int i = 0; i < size; i++)
+            {
+                int pixel1 = (int)(eig_faces[i, num_eig_faces - 1]);
+                min1 = pixel1 < min1 ? pixel1 : min1;
+                max1 = pixel1 > max1 ? pixel1 : max1;
+                int pixel2 = (int)(eig_faces[i, num_eig_faces - 2]);
+                min2 = pixel2 < min2 ? pixel2 : min2;
+                max2 = pixel2 > max2 ? pixel2 : max2;
+                int pixel3 = (int)(eig_faces[i, num_eig_faces - 3]);
+                min3 = pixel3 < min3 ? pixel3 : min3;
+                max3 = pixel3 > max3 ? pixel3 : max3;
+
+            }
+
+
+            for (int column = 0, index = 0; column < image_width; column++)
+            {
+
+                for (int row = 0; row < image_height; row++)
+                {
+                    double p1 = (int)(eig_faces[index, num_eig_faces - 1]);
+                    p1 -= min1;
+                    p1 /= (max1 - min1);
+                    p1 *= 255;
+                    int pixel1 = (int)p1;
+                    double p2 = (int)(eig_faces[index, num_eig_faces - 2]);
+                    p2 -= min2;
+                    p2 /= (max2 - min2);
+                    p2 *= 255;
+                    int pixel2 = (int)p2;
+                    double p3 = (int)(eig_faces[index, num_eig_faces - 3]);
+                    p3 -= min3;
+                    p3 /= (max3 - min3);
+                    p3 *= 255;
+                    int pixel3 = (int)p3;
+
+                    index++;
+
+                    img1.SetPixel(column, row, Color.FromArgb(pixel1, pixel1, pixel1));
+                    img2.SetPixel(column, row, Color.FromArgb(pixel2, pixel2, pixel2));
+                    img3.SetPixel(column, row, Color.FromArgb(pixel3, pixel3, pixel3));
+                    
+                }
+            }
+            
+            pictureBox5.Image = img1;    
+            pictureBox6.Image = img2;
+            pictureBox7.Image = img3;
+                  
+            
+
+        }
+        
+        void recognize(Bitmap img)
+        {
+            Matrix image = new Matrix(size, 1);
+            for (int i = 0, index = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    int value = pixel.R;
+                    image[index, 0] = value - average_face[index, 0];
+                    index++;
+
+                }
+            }
+            Matrix features = (Matrix)image.Transpose().Multiply(eig_faces);
+            Matrix reconstruct = (Matrix)eig_faces.Multiply(features.Transpose());
+            Bitmap reconstructed_image = new Bitmap(image_width, image_height);
+
+            int best1 = 0;
+            double error1 = int.MaxValue;
+            int best2 = 0;
+            double error2 = int.MaxValue;
+            int best3 = 0;
+            double error3 = int.MaxValue;
+
+            for (int k = 0; k < number_of_images; k++)
+            {
+                Matrix result = (Matrix)features.Subtraction(image_features[k]);
+                double error = 0;
+                for (int i = 0; i < num_eig_faces; i++)
+                {
+                    error += result[0, i] * result[0, i];
+                }
+                error = Math.Sqrt(error);
+                if (error < error1)
+                {
+                    best3 = best2;
+                    error3 = error2;
+
+                    best2 = best1;
+                    error2 = error1;
+
+                    best1 = k;
+                    error1 = error;
+
+                }
+                else if (error < error2)
+                {
+                    best3 = best2;
+                    error3 = error2;
+
+                    best2 = k;
+                    error2 = error;
+
+                }
+                else if (error < error3)
+                {
+                    best3 = k;
+                    error3 = error;
+                }
+
+            }
+
+
+            Bitmap image1 = new Bitmap(image_width, image_height);
+            Bitmap image2 = new Bitmap(image_width, image_height);
+            Bitmap image3 = new Bitmap(image_width, image_height);
+
+            long min = int.MaxValue;
+            long max = int.MinValue;
+
+            for(int i = 0; i< size; i++)
+            {
+                int p = (int)reconstruct[i, 0];
+                min = p < min ? p : min;
+                max = p > max ? p : max;
+            }
+
+            for (int i = 0, index = 0; i < image_width; i++)
+            {
+                for (int j = 0; j < image_height; j++)
+                {
+                    int pixel1 = (int)(I[index, best1] + average_face[index, 0]);
+                    int pixel2 = (int)(I[index, best2] + average_face[index, 0]);
+                    int pixel3 = (int)(I[index, best3] + average_face[index, 0]);
+                    double pR = (int)reconstruct[index, 0];
+                    pR -= min;
+                    pR /= (max - min);
+                    pR *= 255;
+                    int pixelR = (int)pR;
+                    index++;
+
+                    image1.SetPixel(i, j, Color.FromArgb(pixel1, pixel1, pixel1));
+                    image2.SetPixel(i, j, Color.FromArgb(pixel2, pixel2, pixel2));
+                    image3.SetPixel(i, j, Color.FromArgb(pixel3, pixel3, pixel3));
+                    reconstructed_image.SetPixel(i, j, Color.FromArgb(pixelR, pixelR, pixelR));
+
+                }
+            }
+            pictureBox2.Image = image1;
+            textBox5.Text = "Error: " + error1;
+            pictureBox3.Image = image2;
+            textBox6.Text = "Error: " + error2;
+            pictureBox4.Image = image3;
+            textBox7.Text = "Error: " + error3;
+            pictureBox8.Image = reconstructed_image;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            loadImages();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpeg files (*.jpg)|*.jpg|(*.gif)|gif||";
+
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                Bitmap img = new Bitmap(dialog.FileName);
+                pictureBox1.Image = img;
+                recognize(img);
+            }
+
         }
     }
 }
