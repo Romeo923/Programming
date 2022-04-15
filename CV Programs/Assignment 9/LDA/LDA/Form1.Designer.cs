0c19cf616490a090f40cd6ea48f45081827c40e1
MrSuave <cromeo5112@gmail.com>
2022-04-15 14:13:31 -0400

    ll

---
+++ CV Programs/Assignment 9/LDA/LDA/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace LDA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_image = new System.Windows.Forms.PictureBox();
+            this.match1 = new System.Windows.Forms.PictureBox();
+            this.match2 = new System.Windows.Forms.PictureBox();
+            this.match3 = new System.Windows.Forms.PictureBox();
+            this.proj1 = new System.Windows.Forms.PictureBox();
+            this.proj2 = new System.Windows.Forms.PictureBox();
+            this.proj3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.input_image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input_image
+            // 
+            this.input_image.Location = new System.Drawing.Point(12, 70);
+            this.input_image.Name = "input_image";
+            this.input_image.Size = new System.Drawing.Size(464, 628);
+            this.input_image.TabIndex = 0;
+            this.input_image.TabStop = false;
+            // 
+            // match1
+            // 
+            this.match1.Location = new System.Drawing.Point(574, 70);
+            this.match1.Name = "match1";
+            this.match1.Size = new System.Drawing.Size(270, 311);
+            this.match1.TabIndex = 1;
+            this.match1.TabStop = false;
+            // 
+            // match2
+            // 
+            this.match2.Location = new System.Drawing.Point(915, 70);
+            this.match2.Name = "match2";
+            this.match2.Size = new System.Drawing.Size(270, 311);
+            this.match2.TabIndex = 2;
+            this.match2.TabStop = false;
+            // 
+            // match3
+            // 
+            this.match3.Location = new System.Drawing.Point(1265, 70);
+            this.match3.Name = "match3";
+            this.match3.Size = new System.Drawing.Size(270, 311);
+            this.match3.TabIndex = 3;
+            this.match3.TabStop = false;
+            // 
+            // proj1
+            // 
+            this.proj1.Location = new System.Drawing.Point(574, 387);
+            this.proj1.Name = "proj1";
+            this.proj1.Size = new System.Drawing.Size(270, 311);
+            this.proj1.TabIndex = 4;
+            this.proj1.TabStop = false;
+            // 
+            // proj2
+            // 
+            this.proj2.Location = new System.Drawing.Point(915, 387);
+            this.proj2.Name = "proj2";
+            this.proj2.Size = new System.Drawing.Size(270, 311);
+            this.proj2.TabIndex = 5;
+            this.proj2.TabStop = false;
+            // 
+            // proj3
+            // 
+            this.proj3.Location = new System.Drawing.Point(1265, 387);
+            this.proj3.Name = "proj3";
+            this.proj3.Size = new System.Drawing.Size(270, 311);
+            this.proj3.TabIndex = 6;
+            this.proj3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1769, 785);
+            this.Controls.Add(this.proj3);
+            this.Controls.Add(this.proj2);
+            this.Controls.Add(this.proj1);
+            this.Controls.Add(this.match3);
+            this.Controls.Add(this.match2);
+            this.Controls.Add(this.match1);
+            this.Controls.Add(this.input_image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.input_image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.match3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proj3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox input_image;
+        private System.Windows.Forms.PictureBox match1;
+        private System.Windows.Forms.PictureBox match2;
+        private System.Windows.Forms.PictureBox match3;
+        private System.Windows.Forms.PictureBox proj1;
+        private System.Windows.Forms.PictureBox proj2;
+        private System.Windows.Forms.PictureBox proj3;
+    }
+}
+
---
+++ DL Programs/Assignment 8/ClassifierModel.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class ClassifierModel(nn.Module):
+    def __init__(self):
+        super(ClassifierModel, self).__init__()
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        # load already trained Siamese network
+        self.model_siamese = torch.load('DL Programs/Assignment 8/checkpoint/finalsiamese.tar')
+        # freeze weights and biases of Siamese network
+        for param in self.model_siamese.parameters():
+            param.requires_grad = False
+            self.classifier = nn.Sequential(
+                nn.Linear(2, 50), # first number should match the latent dimension
+                nn.ReLU(), # i.e., embedding from Siamese
+                nn.Linear(50, 10),
+                nn.Softmax(dim=1)
+            ).to(device)
+        for param in self.classifier.parameters():
+            param.requires_grad = True
+
+    def forward(self,x):
+        h = self.model_siamese(x)
+        return self.classifier(h)
+
